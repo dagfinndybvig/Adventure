6b6673b3cc6a6f6705bda3f2b387d6c56dad65f4
Dagfinn Dybvig <dagfinn.dybvig@gmail.com>
2018-05-30 12:56:32 +0200

    Added class for things

--- Game.kt
+++ Game.kt
@@ -1,24 +1,38 @@
 //Game.kt
 
 import Room
+import Thing
+
+//---Rooms
 
 	val noway=Room(0,"limbo","no exit",0,0,0,0)
 	val dungeon=Room(1,"the dungeon","dark and cobwebby",3,0,0,4)
 	val attic=Room(2,"the attic","dusty, long forgotten",0,0,0,3)
 	val entrance=Room(3,"the once resplendent entrance hall","reduced to a shadow of former glory",0,1,2,0)
 	val tunnel=Room(4,"a tunnel","a twisty passage",0,0,1,3)
+	val player=Room(999,"the player","inventory",0,0,0,0)
 
 	val rooms = hashMapOf(0 to noway,
 			    1 to dungeon, 
 			    2 to attic,
 			    3 to entrance,
-			    4 to tunnel)
+			    4 to tunnel,
+			    999 to  player)
+	var current_room = 3 //Start at entrance
 
-var current_room = 3 //Start at entrance
+
+//---Things
+
+	val sword=Thing(1,"a sword","still sharp",2)
+	val lamp=Thing(2,"a lamp","still contains oil",2)
+
+	val things = hashMapOf(1 to sword,
+			       2 to lamp)
+	val nr_things = 2
 
 
 fun get_input(){
-val inn = readLine()!!
+	var inn = readLine()!!
 	if (inn=="e") current_room=rooms[current_room]!!.e
 	if (inn=="w") current_room=rooms[current_room]!!.w
 	if (inn=="n") current_room=rooms[current_room]!!.n
@@ -38,8 +52,11 @@ println()
 
 while (current_room!=0){
 	rooms[current_room]!!.describe()
+	for (n in 1..nr_things) if (things[n]!!.loc==current_room) things[n]!!.describe()
+	println()
 	get_input()
 	}
+
 println("            Goodbye!")
 println()
 }
